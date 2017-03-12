d30415f8da80dbc4590a51b4928091b6481a4bef
musisiofuganda <musisiofuganda@gmail.com>
2017-03-12 14:09:01 +0300

    Add files via upload

---
+++ about.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php include('head.php');?>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top">
+         <ul class="nav"  style="margin-left:120px;">
+      <li><a href="index.php"><i class="icon-home icon-large"></i> Home</a></li>
+      <li><a href="sfp/fst.php"><i class="icon-th-list icon-large"></i> Projects</a></li>
+      <li><a href="gallery.php"><i class="icon-picture icon-large"></i> Gallery</a></li>
+     <li class="active"><a href="latest_videos"><i class="icon-book icon-large"></i>Videos</a></li>
+      <li><a href="contact.php"><i class="icon-phone icon-large"></i> Contact Us</a></li>
+    </ul>
+      <div class="navbar-inner">
+        <div class="container">
+
+        
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+	
+
+      <!-- Main hero unit for a primary marketing message or call to action -->
+	  <br>
+		
+	  <center><div class="navbar" style="">
+  <div class="navbar-inner" style="margin-top:35px; background-color:#cc6055; width:500px;">
+      <center><a href="index.php"><img src="sfp/iuea/fst.png" alt="LOGO" width="650px" height="80"/></a></center>
+    
+  </div>
+          </div></center>
+	  
+      <!-- Example row of columns -->
+      <div class="row">
+        <div class="span9">
+          <div class="latest"><i class="icon-book icon-large"></i> About Us</div>
+		  
+          <p style=" line-height:30px;">
+		  
+		  Hello, welcome to Website for students Projects. This is the official Website for handling final year students projects with their documenttion at International University of East Africa. 
+<br>
+Office address: 
+Next to IUEA Board room<br><br>
+		Started:	2016<br>
+<br>
+Open to students:	Mon - Wed: 9:00 am - 11:00 pm<br>
+1:00 pm :Out for Lunch<br>
+<br><br>
+
+
+
+For the research & enhancinced project work
+<br><br>
+
+<center><b>Vision</b></center><br>
+<p style=" line-height:30px;">
+By bringing projects information close to students  , we will help:<br><br>
+ 1. increase on the level of research concerning projecct topics, and <br><br>2. enlighten students about all project progresses. <br><br>
+</p><br><br>
+
+
+<center><b>Mission </b></center><br>
+<p style=" line-height:30px;">
+1. To be posted soon.<br>
+
+
+		  </p>
+				
+        </div>
+        <div class="span3"><br>
+		   <div class="green">  <center><p style="font-size:18px;"><b>Site Map</b></center></div>
+		  		  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7686375830285!2d32.60558191431897!3d0.28593306415027886!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177dbc3500f9c5af%3A0x3dc32a101d6f97d!2sInternational+University+Of+East+Africa!5e0!3m2!1sen!2s!4v1487245922421" width="290" height="300" frameborder="0" style="border:2px solid #ccc"></iframe> 	
+
+		  <hr>
+		  <a href="https://www.facebook.com/"><img src="images/bnr_facebook.png"></a><br><br>
+		   <a href="https://www.twitter.com/"><img src="images/bnr_twitter.png"></a>
+		  <hr>
+        </div>
+      </div>
+
+<?php include('footer.php');?>
